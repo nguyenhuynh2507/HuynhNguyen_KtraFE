57be5a309ff1d1dbef1bf703eb9168afe3dfca2d
ITZNGUEN <ITZNGUEN@MSI>
2024-10-07 19:57:31 +0700

    Add project files.

---
+++ KTRAFE/Controllers/LobbyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTRAFE.Controllers
+{
+    public class LobbyController : Controller
+    {
+        // GET: Lobby
+        public ActionResult MainLobby()
+        {
+            return View();
+        }
+    }
+}
---
+++ KTRAFE/Controllers/ShipipingAddressController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTRAFE.Controllers
+{
+    public class ShipipingAddressController : Controller
+    {
+        // GET: ShipipingAddress
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShippingAddress()
+        {
+            return View();
+        }
+    }
+}
